4b0e4f28dd7de1e79668fa025c007eed90585500
Suresh <asuresh@thisisswitch.com>
2019-01-28 18:07:30 +0530

    sgfsdg

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,6 +6,6 @@ array=(${string//,/ })
 #Iterate servers for deploy and pull last commit
 for i in "${!array[@]}"; do    
         echo "Deploy project on server ${array[i]}"    
-        ssh ubuntu@${array[i]} "cd /var/opt/gitlab/nginx/html/test/angularcli && ls && sudo git pull origin master && sudo npm install && ./node_modules/@angular/cli/bin/ng build  && ls && pwd && sudo cp -R /var/opt/gitlab/nginx/html/test/angularcli/dist/* /var/opt/gitlab/nginx/html/angulerTest/ && ls"
+        ssh ubuntu@${array[i]} "cd /var/opt/gitlab/nginx/html/test/angularcli && ls && sudo git pull origin master && sudo npm install && sudo ./node_modules/@angular/cli/bin/ng build  && ls && pwd && sudo cp -R /var/opt/gitlab/nginx/html/test/angularcli/dist/udemy/* /var/opt/gitlab/nginx/html/angulerTest/ && sudo cp -R /var/opt/gitlab/nginx/html/test/index.html /var/opt/gitlab/nginx/html/angulerTest/ && ls"
 done
 
