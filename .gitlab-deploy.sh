a1504c8312e2cb4539cbe5fae2d13e77ce67c5b1
Suresh <asuresh@thisisswitch.com>
2019-01-28 17:24:52 +0530

    sdfsdfsd

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,6 +6,6 @@ array=(${string//,/ })
 #Iterate servers for deploy and pull last commit
 for i in "${!array[@]}"; do    
         echo "Deploy project on server ${array[i]}"    
-        ssh ubuntu@${array[i]} "cd /home/ubuntu/nodeProjects/cciapi/cciapi && git pull gitlab master && forever restart -c node ."
+        ssh ubuntu@${array[i]} "cd /var/opt/gitlab/nginx/html/test/angularcli && git pull gitlab master && sudo npm install && ./node_modules/@angular/cli/bin/ng build  && ls && pwd && sudo cp -R /home/gitlab-runner/builds/9AjC3Hsk/0/gitlab/srikanth/angularcli/dist/* /var/opt/gitlab/nginx/html/angulerTest/ && ls"
 done
 
